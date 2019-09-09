c9439f86b9b172320b64ea5bdbda512b2a119543
Mirsad Cosovic <mirsad.cosovic@gmail.com>
2019-09-09 18:00:08 +0200

    add print time

--- src/GaussianBP.jl
+++ src/GaussianBP.jl
@@ -1,10 +1,11 @@
 module GaussianBP
 
-export bp, bpn
+export bp, bpn, bptime
 
 using SparseArrays
 using HDF5
 using Random
+using Printf
 
 
 ######################
@@ -41,5 +42,18 @@ include("summation.jl")
 
 include("simplybp.jl")
 include("neumaierbp.jl")
+
+###########
+# BP Time #
+###########
+
+include("printdata.jl")
+
+function bptime(data::String="data_33_14", MAXI::Int64=15, DAMP::Int64=10, PROB::Float64=0.6, ALPH::Float64=0.4, MEAN::Float64=0.0, VARI::Float64=1e3)
+    xbp, fgraph, init, infe, solu = bp(data, MAXI, DAMP, PROB, ALPH, MEAN, VARI)
+    bp_time(fgraph, init, infe, solu)
+
+    return xbp
+end
 
 end # SimplyGBP
--- src/simplybp.jl
+++ src/simplybp.jl
@@ -1,34 +1,42 @@
 function bp(data::String="data_33_14", MAXI::Int64=15, DAMP::Int64=10, PROB::Float64=0.6, ALPH::Float64=0.4, MEAN::Float64=0.0, VARI::Float64=1e3)
-    H, b, v = model(data)                                                                                      # input.jl
+    H, b, v = model(data)                                                                                               # input.jl
 
-    Nf, Nv, T = graph(H)                                                                                       # factorgraph.jl
-    Nld, Nli, dir = links(Nf, T)                                                                               # factorgraph.jl
-    vir = virtuals(Nv, dir)                                                                                    # factorgraph.jl
-    Ii, Ji, Ni, bi, vi, Hi, md, vid = factors(Nf, Nv, Nld, Nli, T, b, v, vir, MEAN, VARI)                      # factorgraph.jl
-
-    m_fv, vi_fv, m_vf, v_vf = load_messages(Hi)                                                                # auxiliary.jl
-    ah1, ah2 = damping(Nli, ALPH, PROB)                                                                        # initialize.jl
-    m_vf, v_vf = forward_directs(Hi, Ji, Nli, md, vid, v_vf, m_vf)                                             # initialize.jl
-
-    msr, vsr, msc, vsc = load_sum(Nv, Ni)                                                                      # auxiliary.jl
-    msr, vsr = sum_rows(Hi, Ii, Nli, m_vf, v_vf, msr, vsr)                                                     # summation.jl
-    m_fv, vi_fv = factor_to_variable(m_vf, v_vf, m_fv, vi_fv, msr, vsr, Hi, bi, vi, Ii, Nli)                   # inference.jl
-    msr, vsr, msc, vsc = clear_sum(msr, vsr, msc, vsc)                                                         # auxiliary.jl
-
-for i = 1:MAXI
-    msc, vsc = sum_cols(Ji, Nli, m_fv, vi_fv, msc, vsc)                                                         # summation.jl
-    m_vf, v_vf = variable_to_factor(m_vf, v_vf, m_fv, vi_fv, md, vid, msc, vsc, Ji, Nli)                        # inference.jl
-    msr, vsr = sum_rows(Hi, Ii, Nli, m_vf, v_vf, msr, vsr)                                                      # summation.jl
-    if i < DAMP
-        m_fv, vi_fv = factor_to_variable(m_vf, v_vf, m_fv, vi_fv, msr, vsr, Hi, bi, vi, Ii, Nli)                # inference.jl
-    else
-        m_fv, vi_fv = dfactor_to_variable(m_vf, v_vf, m_fv, vi_fv, msr, vsr, Hi, bi, vi, Ii, Nli, ah1, ah2)     # inference.jl
+    fgraph = @elapsed begin
+        Nf, Nv, T = graph(H)                                                                                            # factorgraph.jl
+        Nld, Nli, dir = links(Nf, T)                                                                                    # factorgraph.jl
+        vir = virtuals(Nv, dir)                                                                                         # factorgraph.jl
+        Ii, Ji, Ni, bi, vi, Hi, md, vid = factors(Nf, Nv, Nld, Nli, T, b, v, vir, MEAN, VARI)                           # factorgraph.jl
     end
-    msr, vsr, msc, vsc = clear_sum(msr, vsr, msc, vsc)                                                          # auxiliary.jl
-end
 
-    msc, vsc = sum_cols(Ji, Nli, m_fv, vi_fv, msc, vsc)                                                         # summation.jl
-    xbp = marginal(md, vid, msc, vsc, Ji, Nv)                                                                   # inference.jl
+    init = @elapsed begin
+        m_fv, vi_fv, m_vf, v_vf = load_messages(Hi)                                                                     # auxiliary.jl
+        ah1, ah2 = damping(Nli, ALPH, PROB)                                                                             # initialize.jl
+        m_vf, v_vf = forward_directs(Hi, Ji, Nli, md, vid, v_vf, m_vf)                                                  # initialize.jl
 
-    return xbp
+        msr, vsr, msc, vsc = load_sum(Nv, Ni)                                                                           # auxiliary.jl
+        msr, vsr = sum_rows(Hi, Ii, Nli, m_vf, v_vf, msr, vsr)                                                          # summation.jl
+        m_fv, vi_fv = factor_to_variable(m_vf, v_vf, m_fv, vi_fv, msr, vsr, Hi, bi, vi, Ii, Nli)                        # inference.jl
+        msr, vsr, msc, vsc = clear_sum(msr, vsr, msc, vsc)                                                              # auxiliary.jl
+    end
+
+    infe = @elapsed begin
+        for i = 1:MAXI
+            msc, vsc = sum_cols(Ji, Nli, m_fv, vi_fv, msc, vsc)                                                         # summation.jl
+            m_vf, v_vf = variable_to_factor(m_vf, v_vf, m_fv, vi_fv, md, vid, msc, vsc, Ji, Nli)                        # inference.jl
+            msr, vsr = sum_rows(Hi, Ii, Nli, m_vf, v_vf, msr, vsr)                                                      # summation.jl
+            if i < DAMP
+                m_fv, vi_fv = factor_to_variable(m_vf, v_vf, m_fv, vi_fv, msr, vsr, Hi, bi, vi, Ii, Nli)                # inference.jl
+            else
+                m_fv, vi_fv = dfactor_to_variable(m_vf, v_vf, m_fv, vi_fv, msr, vsr, Hi, bi, vi, Ii, Nli, ah1, ah2)     # inference.jl
+            end
+            msr, vsr, msc, vsc = clear_sum(msr, vsr, msc, vsc)                                                          # auxiliary.jl
+        end
+    end
+
+    solu = @elapsed begin
+        msc, vsc = sum_cols(Ji, Nli, m_fv, vi_fv, msc, vsc)                                                             # summation.jl
+        xbp = marginal(md, vid, msc, vsc, Ji, Nv)                                                                       # inference.jl
+    end
+
+    return xbp, fgraph, init, infe, solu
  end
