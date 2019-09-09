e8991c9d5a559fe93349a80d74a33421b227c7c4
Mirsad Cosovic <mirsad.cosovic@gmail.com>
2019-09-09 22:06:13 +0200

    fixed small errors

--- test/SimpleTest.jl
+++ test/SimpleTest.jl
@@ -5,28 +5,20 @@ using SparseArrays
 using HDF5
 using Test
 
-bp("SimpleTest", 20, 10, 0.6, 0.4, 0.0, 1e3, ALGORITHM = "sum", ERROR = "on", PATH = "test/")
+function wlsMldivide(H, b, v)
+    W = spdiagm(0 =>  @. 1.0 / sqrt(v))
+    Hti = W * H
+    rti = W * b
 
+    xml = (Hti' * Hti) \ (Hti' * rti)
 
-# function wlsMldivide(H, b, v)
-#     W = spdiagm(0 =>  @. 1.0 / sqrt(v))
-#     Hti = W * H
-#     rti = W * b
-#
-#     xml = (Hti' * Hti) \ (Hti' * rti)
-#
-#     return xml
-# end
+    return xml
+end
 
+xbp, H, b, v = bp("SimpleTest", 1000, 10, 0.6, 0.4, 0.0, 1e6, ALGORITHM = "sum", PATH = "test/")
+println(@test xbp ≈ wlsMldivide(H, b, v))
 
-
-# xml = wlsMldivide(H, b, v)
-#
-# a = @test bp("SimpleTest", 1000, 10, 0.6, 0.4, 0.0, 1e6)  ≈ xml
-# b = @test bpn("SimpleTest", 1000, 10, 0.6, 0.4, 0.0, 1e60)  ≈ xml
-# println(a)
-# println(b)
-
-
+xbp, H, b, v = bp("SimpleTest", 1000, 10, 0.6, 0.4, 0.0, 1e60, ALGORITHM = "kahan", PATH = "test/")
+println(@test xbp ≈ wlsMldivide(H, b, v))
 
 end # SimpleTest
