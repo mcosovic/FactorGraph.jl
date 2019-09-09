1e33d2c08cfd110194624d707d396694ab1c5f69
Mirsad Cosovic <mirsad.cosovic@gmail.com>
2019-09-09 18:24:39 +0200

    fixed main module

--- src/GaussianBP.jl
+++ src/GaussianBP.jl
@@ -27,30 +27,4 @@ include("evaluation.jl")
 include("simplybp.jl")
 include("neumaierbp.jl")
 
-###########
-# BP Time #
-###########
-
-
-# function bptime(data::String="data_33_14", MAXI::Int64=15, DAMP::Int64=10, PROB::Float64=0.6, ALPH::Float64=0.4, MEAN::Float64=0.0, VARI::Float64=1e3)
-#     xbp, fgraph, init, infe, solu = bp(data, MAXI, DAMP, PROB, ALPH, MEAN, VARI)
-#     bp_time(fgraph, init, infe, solu)
-#
-#     return xbp
-# end
-#
-# function bpntime(data::String="data_33_14", MAXI::Int64=15, DAMP::Int64=10, PROB::Float64=0.6, ALPH::Float64=0.4, MEAN::Float64=0.0, VARI::Float64=1e3)
-#     xbp, fgraph, init, infe, solu = bpn(data, MAXI, DAMP, PROB, ALPH, MEAN, VARI)
-#     bp_time(fgraph, init, infe, solu)
-#
-#     return xbp
-# end
-
-#############
-# BP vs WLS #
-#############
-
-
-
-
 end # SimplyGBP
